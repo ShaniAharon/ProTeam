d7b0601dfc4656ced74b065726f4120ca189eddd
shforeal <rivena14798@gmail.com>
2022-03-22 22:15:06 +0200

    'some-style-home-page'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-  import TheWelcome from '@/components/TheWelcome.vue';
-</script>
+<script></script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
+    <h1>Hello</h1>
+    <header>
+      <div class="flex home-view space-between container-layout">
+        <div class="logo">Logo</div>
+        <div>empty places to stay</div>
+        <div>Become a host</div>
+      </div>
+    </header>
   </main>
 </template>
