0f0cb35ed4f54df6d7b89ea1f5240c07748ed917
shforeal <rivena14798@gmail.com>
2022-03-24 09:57:28 +0200

    added style to google form page

--- src/views/google-sheet-form.vue
+++ src/views/google-sheet-form.vue
@@ -1,13 +1,19 @@
 <template>
-  <section>
+  <section class="column container-center form-width">
     <h1>Google Sheet form page</h1>
     <input
       type="text"
+      class="input-big"
       v-model="formData.name"
       placeholder="Enter project name"
     />
-    <input type="text" v-model="formData.price" placeholder="Enter price" />
-    <button @click="onAdd">Add</button>
+    <input
+      class="input-big"
+      type="text"
+      v-model="formData.price"
+      placeholder="Enter price"
+    />
+    <button class="btn btn-black" @click="onAdd">Add</button>
   </section>
 </template>
 
